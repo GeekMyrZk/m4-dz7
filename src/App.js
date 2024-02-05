cee61bab9157c62f94e5895d92b7fac9e7c7663f
GeekMyrZk <dlaprogramminga@gmail.com>
2024-02-05 13:13:02 +0600

    dz7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useCallback } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ExampleMemo = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    const squaredValue = useMemo(() => {
+        console.log('Calculating squared value...');
+        return inputValue * inputValue;
+    }, [inputValue]);
+
+    return (
+        <div>
+            <input
+                type="number"
+                value={inputValue}
+                onChange={(e) => setInputValue(Number(e.target.value))}
+            />
+            <p>Squared Value: {squaredValue}</p>
+        </div>
+    );
+};
+
+const ExampleCallback = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = useCallback(() => {
+        setCount((prevCount) => prevCount + 1)
+    }, [])
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+        </div>
+    );
+};
+
+export default function App(){
+    return <>
+        <ExampleMemo />
+        <hr/>
+        <ExampleCallback />
+    </>
 }
-
-export default App;
